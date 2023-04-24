b303a741fd805e68d0bc89e383cf9cb1fe2d73ee
Garfield Lee <Garfield550@users.noreply.github.com>
2023-04-24 09:27:47 +0000

    refactor: always remove temporary directory

--- src/tasks/deploy-gradio.ts
+++ src/tasks/deploy-gradio.ts
@@ -39,62 +39,69 @@ export async function deployGradio({
   )
   logger.debug(`Temporary directory: ${codeDirectory}`)
 
-  // Clone repository
-  const git = new GitService({ codeDirectory })
-  logger.info(`Cloning repository: ${githubUrl}`)
-  await git.clone(githubUrl)
+  try {
+    // Clone repository
+    const git = new GitService({ codeDirectory })
+    logger.info(`Cloning repository: ${githubUrl}`)
+    await git.clone(githubUrl)
 
-  // Copy Dockerfile
-  const dockerFilePath = path.join(__dirname, '../../scripts/Dockerfile')
-  logger.info(`Copying Dockerfile ${dockerFilePath} to: ${codeDirectory}`)
-  await fsHelper.cp(dockerFilePath, codeDirectory)
+    // Copy Dockerfile
+    const dockerFilePath = path.join(__dirname, '../../scripts/Dockerfile')
+    logger.info(`Copying Dockerfile ${dockerFilePath} to: ${codeDirectory}`)
+    await fsHelper.cp(dockerFilePath, codeDirectory)
 
-  // Build docker image
-  const kebabOwner = kebabCase(repositoryOwner)
-  const kebabName = kebabCase(repositoryName)
-  const dockerImageName = `${kebabOwner}-${kebabName}-${id}`
-  const internalPort = environment.INITIAL_GRADIO_PORT
-  const docker = new DockerService({
-    codeDirectory,
-    dockerImageName,
-    internalPort,
-  })
-  logger.info(`Building docker image: ${dockerImageName}`)
-  await docker.build()
+    // Build docker image
+    const kebabOwner = kebabCase(repositoryOwner)
+    const kebabName = kebabCase(repositoryName)
+    const dockerImageName = `${kebabOwner}-${kebabName}-${id}`
+    const internalPort = environment.INITIAL_GRADIO_PORT
+    const docker = new DockerService({
+      codeDirectory,
+      dockerImageName,
+      internalPort,
+    })
+    logger.info(`Building docker image: ${dockerImageName}`)
+    await docker.build()
 
-  // Prepare run docker image
-  logger.info(`Checking if docker image is already running: ${dockerImageName}`)
-  const checkOldImage = await docker.ps()
-
-  // Stop and remove docker image
-  if (checkOldImage.ID) {
-    logger.debug(
-      `Docker image ${checkOldImage.Image} state: ${checkOldImage.State}`
+    // Prepare run docker image
+    logger.info(
+      `Checking if docker image is already running: ${dockerImageName}`
     )
-    logger.info(`Stopping docker image: ${dockerImageName}`)
-    await docker.kill(checkOldImage.ID)
+    const checkOldImage = await docker.ps()
 
-    logger.info(`Removing docker image: ${dockerImageName}`)
-    await docker.remove(checkOldImage.ID, true)
+    // Stop and remove docker image
+    if (checkOldImage.ID) {
+      logger.debug(
+        `Docker image ${checkOldImage.Image} state: ${checkOldImage.State}`
+      )
+
+      if (checkOldImage.State === 'running') {
+        logger.info(`Stopping docker image: ${dockerImageName}`)
+        await docker.kill(checkOldImage.ID)
+      }
+
+      logger.info(`Removing docker image: ${dockerImageName}`)
+      await docker.remove(checkOldImage.ID, true)
+    }
+
+    // Run docker image
+    logger.info(`Running docker image: ${dockerImageName}`)
+    await docker.run(port, { CUSTOM_PATH: `/app/${id}` })
+
+    await new Promise((resolve) => setTimeout(resolve, 5000))
+
+    // Check if docker image is running
+    logger.info(`Checking if docker image is running: ${dockerImageName}`)
+    const checkImageRunning = await docker.ps()
+    if (!(checkImageRunning.State === 'running')) {
+      logger.error(`Docker image ${dockerImageName} failed to start`)
+      throw new Error(`Docker image ${dockerImageName} failed to start`)
+    }
+
+    return { port, imageName: dockerImageName }
+  } finally {
+    // Remove temporary directory
+    logger.info(`Removing temporary directory: ${codeDirectory}`)
+    await fsHelper.rmRF(codeDirectory)
   }
-
-  // Run docker image
-  logger.info(`Running docker image: ${dockerImageName}`)
-  await docker.run(port, { CUSTOM_PATH: `/app/${id}` })
-
-  await new Promise((resolve) => setTimeout(resolve, 3000))
-
-  // Check if docker image is running
-  logger.info(`Checking if docker image is running: ${dockerImageName}`)
-  const checkImageRunning = await docker.ps()
-  if (!(checkImageRunning.State === 'running')) {
-    logger.error(`Docker image ${dockerImageName} failed to start`)
-    throw new Error(`Docker image ${dockerImageName} failed to start`)
-  }
-
-  // Remove temporary directory
-  logger.debug(`Removing temporary directory: ${codeDirectory}`)
-  await fsHelper.rmRF(codeDirectory)
-
-  return { port, imageName: dockerImageName }
 }
