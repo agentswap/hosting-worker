8f13f27cbc57b4cfc6b7241cce8b57c8d58a10ac
Garfield Lee <Garfield550@users.noreply.github.com>
2023-05-15 10:38:56 +0000

    fix: systemd output message

--- src/services/caddy/index.ts
+++ src/services/caddy/index.ts
@@ -45,11 +45,7 @@ export class CaddyService {
   public async reload() {
     const { exitCode, stderr, stdout } = await execa(
       'sudo systemctl reload caddy',
-      {
-        reject: false,
-        shell: true,
-        stdio: 'inherit',
-      }
+      { reject: false, shell: true }
     )
     if (exitCode !== 0) {
       logger.error(`Error reloading caddy: ${stderr}`)
@@ -61,11 +57,7 @@ export class CaddyService {
   public async restart() {
     const { exitCode, stderr, stdout } = await execa(
       'sudo systemctl restart caddy',
-      {
-        reject: false,
-        shell: true,
-        stdio: 'inherit',
-      }
+      { reject: false, shell: true }
     )
     if (exitCode !== 0) {
       logger.error(`Error restarting caddy: ${stderr}`)
